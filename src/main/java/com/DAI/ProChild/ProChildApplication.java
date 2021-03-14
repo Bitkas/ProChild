ffb654fb36ebbeb9cc9d158893aa05ae6d36bd78
Bitkas <bitokeiro@gmail.com>
2021-03-14 18:01:58 +0000

    first commit

---
+++ src/main/java/com/DAI/ProChild/ProChildApplication.java
@@ -0,0 +1,13 @@
+package com.DAI.ProChild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProChildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProChildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DAI/ProChild/ProChildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAI.ProChild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProChildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
