4d6ee66d27fb585e64ca501c65c791decf19642e
Bitkas <bitokeiro@gmail.com>
2021-03-25 19:28:19 +0000

    added @Repository

--- src/main/java/com/DAI/ProChild/Topic/TopicRepository.java
+++ src/main/java/com/DAI/ProChild/Topic/TopicRepository.java
@@ -1,6 +1,8 @@
 package com.DAI.ProChild.Topic;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TopicRepository extends JpaRepository<Topic, Integer> {
 }
