be24b6dc5b1bdc6cf6b286a7d59e8ad3aa1b1b49
Bitkas <bitokeiro@gmail.com>
2021-03-25 19:27:50 +0000

    added @Repository

--- src/main/java/com/DAI/ProChild/Complaint_form/ComplaintFormRepository.java
+++ src/main/java/com/DAI/ProChild/Complaint_form/ComplaintFormRepository.java
@@ -1,6 +1,8 @@
 package com.DAI.ProChild.Complaint_form;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ComplaintFormRepository extends JpaRepository<Complaint_Form, Integer> {
 }
