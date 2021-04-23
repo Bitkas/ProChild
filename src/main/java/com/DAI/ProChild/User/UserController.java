500de0b43987574485ba27c2e3dd99421b0b9440
bitkas <bitokeiro@gmail.com>
2021-04-23 17:57:55 +0100

    test cors

--- src/main/java/com/DAI/ProChild/User/UserController.java
+++ src/main/java/com/DAI/ProChild/User/UserController.java
@@ -17,6 +17,7 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(path = "/User/")
     public ResponseEntity<String> getAllUsers() {
         List<User> users = this.userService.GetAllUsers();
