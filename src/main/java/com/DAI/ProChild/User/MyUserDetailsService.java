7ac1ad73c71217ee4d252a0706407aba849d6011
bitkas <bitokeiro@gmail.com>
2021-04-23 18:31:41 +0100

    test cors

--- src/main/java/com/DAI/ProChild/User/MyUserDetailsService.java
+++ src/main/java/com/DAI/ProChild/User/MyUserDetailsService.java
@@ -5,10 +5,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Optional;
 
 @Service
+@RequestMapping(path = "/login/", method = RequestMethod.POST)
 public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
