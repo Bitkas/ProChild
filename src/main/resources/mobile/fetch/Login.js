cb76aa58a78d85bda632ddb6690d6017ccec6ee3
bitkas <bitokeiro@gmail.com>
2021-04-26 17:41:35 +0100

    FrontEnd + BackEnd

---
+++ src/main/resources/mobile/fetch/Login.js
@@ -0,0 +1,47 @@
+//mostrar password
+
+let pswd = false;
+
+function showPswd() {
+    if (pswd) {
+        document.getElementById("passInputEntrar").setAttribute("type", "password");
+        pswd = false;
+    } else {
+        document.getElementById("passInputEntrar").setAttribute("type", "text");
+        pswd = true;
+    }
+}
+
+//login
+
+let login = document.getElementById("EntrarContaBtn");
+login.onclick = myLogin;
+
+function myLogin() {
+    let data = {};
+    data.username = document.getElementById("emailInputEntrar").value;
+    data.password = document.getElementById("passInputEntrar").value;
+    console.log(data); //debugging para ver os dados que foram enviados
+    //chamada fetch para envio dos dados para o servior via POST
+    fetch('http://aqueous-waters-59160.herokuapp.com/login/', {
+        headers: { 'Content-Type': 'application/javascript' },
+        method: 'POST',
+        body: JSON.stringify(data)
+    }).then(function(response) {
+        if (!response.ok) {
+            console.log(response.status); //=> 100-599
+            console.log(response.statusText); //=> String
+            console.log(response.headers); //=> Headers
+            console.log(response.url); //=> String
+            throw Error(response.statusText);
+        } else {
+            console.log("nice");
+            alert("Autenticação feita com sucesso!");
+            window.location.href = "../EscolherArea.html";
+        }
+    }).then(function(result) {
+        console.log(result);
+    }).catch(function(err) {
+        console.error(err);
+    });
+}
