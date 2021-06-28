3951bd2b79ab242f3388e54254e8763945bec466
Bitkas <61255210+Bitkas@users.noreply.github.com>
2021-06-28 19:54:37 +0100

    chat

--- src/main/resources/static/Chat.js
+++ src/main/resources/static/Chat.js
@@ -12,8 +12,13 @@ fetch("/LoggedInUser/", {
         email = result;
 
         //const host = document.location.host;
+        //heroku host
         const host = "aqueous-waters-59160.herokuapp.com"
-        let ws = new WebSocket("wss://" + host +  "/chat/" + email + "/");
+        //heroku web socket
+        let ws = new WebSocket("wss://" + host +  "/chat/" + email);
+        //localhost web socket
+        //let ws = new WebSocket("ws://" + host +  "/chat/" + email);
+
 
         ws.onopen = () => {
             console.log("ola");
